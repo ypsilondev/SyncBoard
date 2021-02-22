9c79586ab047589ee422bad1a44445ebd729fbcc
Niklas @yDev <niklas@magic-computer.de>
2021-02-22 19:58:57 +0100

    Stroke sync timer, inking

--- SyncBoard/MainPage.xaml.cs
+++ SyncBoard/MainPage.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +17,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
@@ -22,9 +28,48 @@ namespace SyncBoard
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<InkStroke> syncedStrokes = new List<InkStroke>();
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Set supported inking device types.
+            inkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen;
+
+            // Set initial ink stroke attributes.
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Black;
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+
+            SynchronizationTask();
+        }
+
+        private async void SynchronizationTask()
+        {
+            Timer timer = new Timer((e) =>
+            {
+                Windows.ApplicationModel.Core.CoreApplication.MainView.Dispatcher.
+                    RunAsync(CoreDispatcherPriority.Low, () =>
+                    {
+                        int toUpdate = 0;
+
+                        foreach (var stroke in inkCanvas.InkPresenter.StrokeContainer.GetStrokes())
+                        {
+                            if (!syncedStrokes.Contains(stroke))
+                            {
+                                toUpdate++;
+                                syncedStrokes.Add(stroke);
+                            }
+                        }
+
+                        System.Diagnostics.Debug.WriteLine(toUpdate);
+                    });
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
         }
     }
 }
